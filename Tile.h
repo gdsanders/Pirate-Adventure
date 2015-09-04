00e79d62997bd42a642ad26afec8c23367e8eef5
gdsanders <gd@digitalequity.com>
2015-09-04 17:05:49 -0500

    Tile class created

---
+++ Tile.h
@@ -0,0 +1,19 @@
+//
+//  Tile.h
+//  Pirate Adventure
+//
+//  Created by G.D. Sanders on 9/4/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Tile : NSObject
+
+@property (strong, nonatomic) NSString *story;
+@property (strong, nonatomic) UIImage *backgroundImage;
+@property (strong, nonatomic) NSString *actionButtonName;
+
+
+@end
---
+++ Tile.m
@@ -0,0 +1,13 @@
+//
+//  Tile.m
+//  Pirate Adventure
+//
+//  Created by G.D. Sanders on 9/4/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import "Tile.h"
+
+@implementation Tile
+
+@end
